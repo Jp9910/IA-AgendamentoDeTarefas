85945177203f866e3c18aba9137f40cd30707a0b
jeremiassnts3@gmail.com <jeremiassnts3@gmail.com>
2021-07-07 16:51:30 -0300

    feat: caso 1

--- aima-gui/src/main/java/aima/gui/projetofinal/AgendamentoCSP.java
+++ aima-gui/src/main/java/aima/gui/projetofinal/AgendamentoCSP.java
@@ -9,73 +9,113 @@ import aima.core.search.csp.examples.NotEqualConstraint;
 import aima.gui.projetofinal.Variaveis;
 import aima.gui.projetofinal.ConstraintTodasAtividadesAlocadas;
 
-public class AgendamentoCSP extends CSP<Variable, String>{
+public class AgendamentoCSP extends CSP<Variable, String> {
 
-	//Integer[] array = new Integer[100];
-	//List<Variable> blocosSeg = new ArrayList<Variable>();
+	// Integer[] array = new Integer[100];
+	// List<Variable> blocosSeg = new ArrayList<Variable>();
 	public static final Variable[] blocos = new Variable[288];
-	public static final String[] dias = {"Seg","Ter","Qua","Qui","Sex","Sab"};
-	
+	public static final String[] dias = { "Seg", "Ter", "Qua", "Qui", "Sex", "Sab" };
+
 	static {
-		int j=0;
+		int j = 0;
 		for (String dia : dias) {
-			for (int i=0;i<48;i=i+2) {
-				blocos[j] = new Variable(dia+" "+(i/2)%24+":00-"+(i/2)%24+":30");
-				blocos[j+1] = new Variable(dia+" "+(i/2)%24+":30-"+((i/2)+1)%24+":00");
-				j=j+2;
+			for (int i = 0; i < 48; i = i + 2) {
+				blocos[j] = new Variable(dia + " " + (i / 2) % 24 + ":00-" + (i / 2) % 24 + ":30");
+				blocos[j + 1] = new Variable(dia + " " + (i / 2) % 24 + ":30-" + ((i / 2) + 1) % 24 + ":00");
+				j = j + 2;
 			}
 		}
 	}
 	public static final String estudar = "estudar";
-	public static final String trabalhar = "trabalhar";
-	public static final String dormir = "dormir";
-	public static final String comer = "comer";
-	public static final String[] atividadesRestantes = {estudar,trabalhar};
-	//public static final String[] dominioDormir = {dormir};
-	
-	public AgendamentoCSP() {
+	public static final String trabalhar = "Estágio/Trabalho";
+	public static final String dormir = "Sono";
+	public static final String comer = "Refeição";
+	public static final String init = "PIBIC/PIBIX/PIBITI";
+	public static final String voluntario = "Atividade voluntária";
+	public static final String palestra = "Palestra/evento extracurricular";
+	public static final String aula = "Horário de aula";
+	public static final String[] atividadesRestantes = { estudar, trabalhar };
+	// public static final String[] dominioDormir = {dormir};
+
+	public AgendamentoCSP(int caso) {
 		super(Arrays.asList(blocos));
-		
-		Domain<String> atividades = new Domain<>(atividadesRestantes); //construtor de Domain funciona com array?
+
+		/*Domain<String> atividades = new Domain<>(atividadesRestantes); // construtor de Domain funciona com array?
 		Domain<String> domDormir = new Domain<>(dormir);
 		Domain<String> domComer = new Domain<>(comer);
-		int i=-1;
-		for (Variable var : getVariables()){
-			i=(i+1)%48;
-			System.out.print(i+"-");
+		int i = -1;
+		for (Variable var : getVariables()) {
+			i = (i + 1) % 48;
+			System.out.print(i + "-");
 			System.out.println(var);
-			if(i<=15) {
-				setDomain(var,domDormir);
-			}
-			else if (i<=17){
-				setDomain(var,domComer);
-			}
-			else
+			if (i <= 15) {
+				setDomain(var, domDormir);
+			} else if (i <= 17) {
+				setDomain(var, domComer);
+			} else
 				setDomain(var, atividades);
+		}*/
+		// addConstraint(new NotEqualConstraint<>(blocos[0],blocos[1]));
+		// addConstraint(new ConstraintTodasAtividadesAlocadas(atividadesRestantes));
+
+		switch (caso) {
+		case 1:
+			Caso1();
+			break;
 		}
-		
-		//addConstraint(new NotEqualConstraint<>(blocos[0],blocos[1]));
-		//addConstraint(new ConstraintTodasAtividadesAlocadas(atividadesRestantes));
-		
+
 	}
-	
-	//main apenas para testar as variáveis
-	/* 
-	 * public static void main(String[] args) {
-		Variable[] blocos = new Variable[288];{
-			//for (int dia=1;dia<=6;dia++) {
-				for (int i=0;i<288;i=i+2) {
-					blocos[i] = new Variable((i/2)%24+":00-"+(i/2)%24+":30");
-					blocos[i+1] = new Variable((i/2)%24+":30-"+((i/2)+1)%24+":00");
-					
+
+	public void Caso1() {
+		var variables = getVariables();
+		// preenche com blocos de materias
+		var blocosEstudo = new ArrayList<String>();
+		blocosEstudo = criarBlocosMateria(8, "COMP0455", blocosEstudo);
+		blocosEstudo = criarBlocosMateria(4, "COMP0481", blocosEstudo);
+		blocosEstudo = criarBlocosMateria(8, "COMP0408", blocosEstudo);
+
+		for (int dia = 0; dia < 6; dia++) {
+			for (int horario = 0; horario < 48; horario++) {
+				Variable var = variables.get((dia * 48) + horario);
+				if (horario >= 46 || horario <= 13) {
+					setDomain(var, new Domain<>(dormir));
+				} else if (horario == 14 || horario == 15 || horario == 24 || horario == 25 || horario == 36
+						|| horario == 37) {
+					setDomain(var, new Domain<>(comer));
+				} else if (horario >= 16 && horario <= 23) {
+					setDomain(var, new Domain<>(trabalhar));
+				} else if (((dia == 1 || dia == 3) && (horario >= 30 && horario <= 33))
+						|| (dia == 3 && (horario >= 38 && horario <= 41))
+						|| ((dia == 0 || dia == 2) && (horario >= 42 && horario <= 45))) {
+					setDomain(var, new Domain<>(aula));
+				} else if (dia == 5 && (horario >= 30 && horario <= 33)) {
+					setDomain(var, new Domain<>(palestra));
+				} else if (dia == 5 && (horario >= 38 && horario <= 45)) {
+					setDomain(var, new Domain<>(voluntario));
+				} else {
+					setDomain(var, new Domain<>(blocosEstudo));
 				}
-			//}
+			}
 		}
-		
-		System.out.println("teste");
-		for (int i=0;i<288;i=i+1) {
-			System.out.println(blocos[i]);
+	}
+
+	private ArrayList<String> criarBlocosMateria(int horas, String codigoMateria, ArrayList<String> blocosEstudo) {
+		for (int i = 0; i < (horas * 2); i++) {
+			blocosEstudo.add("estudo " + codigoMateria + " " + (i + 1));
 		}
-		System.out.println("fim");
-	}*/
+		return blocosEstudo;
+	}
+
+	// main apenas para testar as variáveis
+	/*
+	 * public static void main(String[] args) { Variable[] blocos = new
+	 * Variable[288];{ //for (int dia=1;dia<=6;dia++) { for (int i=0;i<288;i=i+2) {
+	 * blocos[i] = new Variable((i/2)%24+":00-"+(i/2)%24+":30"); blocos[i+1] = new
+	 * Variable((i/2)%24+":30-"+((i/2)+1)%24+":00");
+	 * 
+	 * } //} }
+	 * 
+	 * System.out.println("teste"); for (int i=0;i<288;i=i+1) {
+	 * System.out.println(blocos[i]); } System.out.println("fim"); }
+	 */
 }
--- aima-gui/src/main/java/aima/gui/projetofinal/Demo.java
+++ aima-gui/src/main/java/aima/gui/projetofinal/Demo.java
@@ -14,17 +14,22 @@ import aima.core.search.csp.inference.AC3Strategy;
 public class Demo {
 
 	public static void main(String[] args) {
-		CSP<Variable, String> csp = new AgendamentoCSP();
+		CSP<Variable, String> csp = new AgendamentoCSP(1);
 		CspListener.StepCounter<Variable, String> stepCounter = new CspListener.StepCounter<>();
 		CspSolver<Variable, String> solver;
 		Optional<Assignment<Variable, String>> solution;
-		
+
 		solver = new FlexibleBacktrackingSolver<Variable, String>().set(new AC3Strategy<>());
 		solver.addCspListener(stepCounter);
 		stepCounter.reset();
 		System.out.println("Bloco CSP (Backtracking + AC3)");
 		solution = solver.solve(csp);
-		solution.ifPresent(System.out::println);
+		// solution.ifPresent(System.out::println);
+		for (var v : solution.get().getVariables()) {
+			var variavel = v.getName();
+			var valor = solution.get().getValue(v);
+			System.out.println(variavel + "=" + valor);
+		}
 		System.out.println(stepCounter.getResults() + "\n");
 	}
 }
